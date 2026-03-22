db3164690446b46c0d5d4923bd43878b671b2816
nimantha1999 <nimantha.bt@gmail.com>
2026-03-22 12:29:47 +0530

    Refactor UserController to update getUsers endpoint with error handling and change route to 'get-users'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('get-users')
   getUsers(): string[] {
-    return this.userService.getUsers();
+    // return this.userService.getUsers();
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
   }
 }
